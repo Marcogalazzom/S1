00d018dc0c46d83b3871a49ff60a715eea31aa3d
Marcogalazzom <123545097+Marcogalazzom@users.noreply.github.com>
2023-09-30 09:25:30 +0000

    Create puissance_de_2.py

---
+++ puissance_de_2.py
@@ -0,0 +1,16 @@
+"""
+Objectif du programme : Calcule 2 puissance n (un entier)
+"""
+
+# CONSTANTES 
+
+INT = "Entier ? "
+AFFICHAGE = "2 puissance"
+
+# On demande à l'utilisateur un entier
+
+entier = int(input(INT))
+
+# On affiche le résultat
+
+print(AFFICHAGE, entier, "=", 2**entier)
